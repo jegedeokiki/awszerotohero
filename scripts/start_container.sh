a7b6058e05af2e06a568d576cbf9d097ca706bce
jegedeokiki <110062763+jegedeokiki@users.noreply.github.com>
2025-11-10 16:29:44 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker docker pull jegedeokiki/simple-python-flask-app
+docker pull jegedeokiki/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 jegedeokiki/simple-python-flask-app
